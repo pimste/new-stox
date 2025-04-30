fb7fbc020cf8b10daf5434762c18650adde2d254
pimste <65168095+pimste@users.noreply.github.com>
2025-04-30 11:27:29 +0200

    reset indexation

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const nextConfig = {
     
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
